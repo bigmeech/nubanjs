bee25146f2eb0dcaf26a93b5fc2c1ac2d864ca2d
Larry Eliemenye <denachural@gmail.com>
2021-06-30 20:35:45 +0100

    first commit

---
+++ src/index.js
@@ -0,0 +1,52 @@
+const bankCodes = require('./bankcodes');
+
+const nubanPattern = /(?<bankCode>\d{3})(?<acc>\d{6})(?<check>\d{1})/i
+
+
+/**
+ * takes in a nuban number and returns its constituent parts
+ * @param input
+ * @returns {{bankCode?: string, acc?: string, check?: string}|{[p: string]: string}|*}
+ */
+function parse(input) {
+    if(!nubanPattern.test(input)) {
+        throw new NubanError('Invalid NUBAN string');
+    }
+
+    const match = nubanPattern.exec(input);
+    return match.groups;
+}
+
+/**
+ * take constinient parts and construct a valid nuban number
+ * @param bankCode
+ * @param serialNumber
+ * @returns {string}
+ */
+function stringify(bankCode, serialNumber, separator = null) {
+    if(bankCode.length !== 3) {
+        throw new NubanError('invalid bank code length')
+    }
+
+    if(serialNumber.length !== 6) {
+        throw new NubanError('Invalid account serial number length')
+    }
+
+    return `${bankCode}${separator}${serialNumber}`;
+}
+
+function lookup(nuban) {
+    return bankCodes[nuban.substring(0, 3)]
+}
+
+class NubanError extends Error {
+    constructor(msg) {
+        super(msg);
+    }
+}
+
+module.exports = {
+    parse,
+    stringify,
+    lookup
+}
---
+++ test/index.spec.js
@@ -0,0 +1,80 @@
+const nuban = require('../src');
+describe('v1', () => {
+    describe('parse', () => {
+        test('should return parts of a nuban number', () => {
+            const result = nuban.parse('1234567890');
+            expect(result.bankCode).toBe('123')
+        })
+    })
+    describe('stringify', () => {
+        test('should return a nuban number from parts without separator', () => {
+            const result = nuban.stringify('033', '123456');
+            expect(result).toBeDefined()
+        })
+
+        test('should return a nuban number from parts with / as separator', () => {
+            const result = nuban.stringify('033', '123456', '/');
+            expect(result).toBe('033/123456')
+        })
+
+        test('should return a nuban number from parts with - as separator', () => {
+            const result = nuban.stringify('033', '123456', '-');
+            expect(result).toBe('033-123456')
+        })
+
+    })
+    describe('lookup', () => {
+        test('should take 033 prefix and return United Bank for Africa Plc as name', () => {
+            const result = nuban.lookup('0331234561');
+            expect(result).toBe('United Bank for Africa Plc')
+        })
+        test('should take 044 prefix and return Access Bank Nigeria Plc as name', () => {
+            const result = nuban.lookup('0441234561');
+            expect(result).toBe('Access Bank Nigeria Plc')
+        })
+        test('should take 050 prefix and return Ecobank Nigeria as name', () => {
+            const result = nuban.lookup('0501234561');
+            expect(result).toBe('Ecobank Nigeria')
+        })
+        test('should take 063 prefix and return Diamond Bank Plc as name', () => {
+            const result = nuban.lookup('0631234561');
+            expect(result).toBe('Diamond Bank Plc')
+        })
+        test('should take 084 prefix and return Enterprise Bank Plc as name', () => {
+            const result = nuban.lookup('0841234561');
+            expect(result).toBe('Enterprise Bank Plc')
+        })
+        test('should take 070 prefix and return Fidelity Bank Plc as name', () => {
+            const result = nuban.lookup('0701234561');
+            expect(result).toBe('Fidelity Bank Plc')
+        })
+        test('should take 011 prefix and return First Bank of Nigeria Plc as name', () => {
+            const result = nuban.lookup('0111234561');
+            expect(result).toBe('First Bank of Nigeria Plc')
+        })
+        test('should take 214 prefix and return First City Monument Bank as name', () => {
+            const result = nuban.lookup('2141234561');
+            expect(result).toBe('First City Monument Bank')
+        })
+        test('should take 058 prefix and return Guaranty Trust Bank Plc as name', () => {
+            const result = nuban.lookup('0581234561');
+            expect(result).toBe('Guaranty Trust Bank Plc')
+        })
+        test('should take 030 prefix and return Heritaage Banking Company Ltd as name', () => {
+            const result = nuban.lookup('0301234561');
+            expect(result).toBe('Heritaage Banking Company Ltd')
+        })
+        test('should take 301 prefix and return Jaiz Bank as name', () => {
+            const result = nuban.lookup('3011234561');
+            expect(result).toBe('Jaiz Bank')
+        })
+        test('should take 082 prefix and return Keystone Bank Ltd as name', () => {
+            const result = nuban.lookup('0821234561');
+            expect(result).toBe('Heritaage Banking Company Ltd')
+        })
+        test('should take 082 prefix and return Heritaage Banking Company Ltd as name', () => {
+            const result = nuban.lookup('0821234561');
+            expect(result).toBe('Heritaage Banking Company Ltd')
+        })
+    })
+})
